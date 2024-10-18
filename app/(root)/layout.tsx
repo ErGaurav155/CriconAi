452c9ecdef5b255eb4492f981e2558dc7bf0449c
Guarav Khaire <gauravgkhaire@gmail.com>
2024-10-18 14:31:01 +0530

    plz make it

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
   title: "AI Thumbnail Generator & Tools",
   description:
     "Discover the best free AI tools for generating YouTube thumbnails, downloading in 4K, and more.",
-  keywords: ["free a i image generator"],
+  keywords: ["free ai image generator"],
 };
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
